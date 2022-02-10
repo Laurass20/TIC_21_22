16e6c4056435f46f08e54df78e4434f54a80f179
Laurass20 <90762291+Laurass20@users.noreply.github.com>
2022-02-10 08:56:38 +0100

    Create ManejaNumeros.java

---
+++ ManejaNumeros.java
@@ -0,0 +1,23 @@
+package Micodigo;
+
+public class ManejaNumeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MiParejaNumeros objeto1;//Declaro el objeto
+		objeto1=new MiParejaNumeros(5,8);//Instancio
+		MiParejaNumeros objeto2;
+		objeto2=new MiParejaNumeros(1,4);
+		
+		objeto1.setNum1(6);
+		objeto1.setNum2(4);
+		suma=objeto1.devuelveSuma();
+		objeto2.setNum1(4);
+		objeto2.setNum2(1);
+		System.out.println("Suma de la pareja 1: ");
+		System.out.println(suma);
+		System.out.println("Suma de la pareja 2: ");
+		Sustem.out.println();
+	}
+
+}
