490d706b35bc9107592f6b8e5cd2b18c18aaf12b
Laurass20 <90762291+Laurass20@users.noreply.github.com>
2022-02-14 10:29:47 +0100

    Create Circunferencia.java

---
+++ Circunferencia.java
@@ -0,0 +1,33 @@
+package MCodigo;
+
+public class Circunferencia {
+	//ATRIBUTOS
+	int coordX;
+	int coordY;
+	float radio;
+	//CONSTRUCTOR
+	public Circunferencia(int coordX,int coordY,float radio){
+		this.coordX=coordX;
+		this.coordY=coordY;
+		this.radio=radio;
+	}
+	//MÉTODOS DET GET
+	void setCoordX(int coordX){
+		this.coordX=coordX;
+	}
+	void setCoordY(int coordY){
+		this.coordY=coordY;
+	}
+	void setRadio(float radio){
+		this.radio=radio;
+	}
+	int getCoordX(){
+		return coordX;
+	}
+	int getCoordY(){
+		return coordY;
+	}
+	float getRadio() {
+		return radio;
+	}
+}
