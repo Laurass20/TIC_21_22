9f9b02012cda6d719235e4a5769e61d13c6d84a0
Laurass20 <90762291+Laurass20@users.noreply.github.com>
2022-02-16 12:01:39 +0100

    Create ManejaCircunferencias.java

---
+++ ManejaCircunferencias.java
@@ -0,0 +1,17 @@
+package MiCodigo;
+
+public class ManejaCircunferencias {
+  
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Circunferencia circunferencia1;
+        Circunferencia circunferencia2;
+        circunferencia1=new Circunferencia(7,9,4.5);
+        System.out.println("CIRCULITO 1:");
+        System.out.println("Coordenada X del centro: "+circunferencia1.getCoordX());
+        System.out.println("Coordenada Y del centro: "+circunferencia2.getCoordY());
+        System.out.println("Radio: "+circunferencia1.getRadio());
+        System.out.println("Area "+circunferencia1.devuelevArea());
+    }     
+ }
+        
